975626452484ec0a9feb0c86fda01bf5e84f4919
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-17 19:33:43 +0200

    deleted/moved

--- starter/src/services/content.js
+++
@@ -1,9 +0,0 @@
-import prisma from "../../lib/prismaClient.js";
-
-const getContent = async () => {
-  const content = await prisma.content.findMany();
-
-  return content;
-};
-
-export default getContent;
