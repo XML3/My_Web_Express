1ceea2645f38faed96a52fb7be70e701c8ca92bc
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-17 19:31:47 +0200

    added new route for content id

--- starter/routes/content.js
+++ starter/routes/content.js
@@ -1,5 +1,6 @@
 import express from "express";
-import getContent from "../src/services/content.js";
+import getContent from "../src/services/content/content.js";
+import getContentById from "../src/services/content/getContentById.js";
 
 const router = express.Router();
 
@@ -7,6 +8,22 @@ router.get("/", async (req, res, next) => {
   try {
     const content = await getContent();
     res.status(200).json(content);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Get By Id
+router.get("/:id", async(res, req, next) => {
+  try {
+    const { id } = reg.params:
+    const content = await getContentById(id);
+
+    if(!content) {
+      res.status(404).send(`Content with id ${id} was not found`);
+    } else {
+      res.status(200).json(content);
+    }
   } catch (error) {
     next(error);
   }
