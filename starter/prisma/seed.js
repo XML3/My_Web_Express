f6df1548304a57a08ebcc8411d1172ba32ef8fce
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-17 12:24:49 +0200

    created seed file

---
+++ starter/prisma/seed.js
@@ -0,0 +1,56 @@
+import { PrismaClient } from "@prisma/client";
+import contentData from "../src/data/content.json" assert { type: "json" };
+import aboutData from "../src/data/about.json" assert { type: "json" };
+import portData from "../src/data/port.json" assert { type: "json" };
+import skillData from "../src/data/skills.json" assert { type: "json" };
+
+const prisma = new PrismaClient({ log: ["query,", "info", "warn", "error"] });
+
+async function main() {
+  const { content } = contentData;
+  const { about } = aboutData;
+  const { port } = portData;
+  const { skills } = skillData;
+
+  for (const contents of content) {
+    await prisma.content.upsert({
+      where: { id: contents.id },
+      update: {},
+      create: contents,
+    });
+  }
+
+  for (const abouts of about) {
+    await prisma.about.upsert({
+      where: { id: abouts.id },
+      update: {},
+      create: abouts,
+    });
+  }
+
+  for (const ports of port) {
+    await prisma.port.upsert({
+      where: { id: ports.id },
+      update: {},
+      create: ports,
+    });
+  }
+
+  for (const skill of skills) {
+    await prisma.skills.upsert({
+      where: { id: skill.id },
+      update: {},
+      create: skill,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
