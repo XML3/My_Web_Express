bfa7b9360b1efc91428f1b53d69563d0bc04602f
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-20 00:35:08 +0200

    port back to 3000, CORS to actual domain

--- starter/src/index.js
+++ starter/src/index.js
@@ -69,8 +69,8 @@ app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorHandler);
 
-const port = process.env.POST || 3000;
-app.listen(3000, () => {
+const port = process.env.POST || 4000;
+app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
