fa93e1258924dea30a7617aaaf0f0275a0759776
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-20 00:36:31 +0200

    added checks to find unique email to avoid repeating existing address

--- starter/src/services/contactForm.js
+++ starter/src/services/contactForm.js
@@ -2,7 +2,17 @@ import prisma from "../../lib/prismaClient.js";
 
 const contactFormEntry = async (firstName, lastName, email, message) => {
   try {
-    const newcontact = await prisma.contact.create({
+    //Checks if emails address already exists in the DB
+    const existingContact = await prisma.contact.findUnique({
+      where: { email: email },
+    });
+
+    if (existingContact) {
+      throw new Error("A contact with this email already exists.");
+    }
+
+    //Create a new contact in the DB
+    const newContact = await prisma.contact.create({
       data: {
         firstName: firstName,
         lastName: lastName,
@@ -10,9 +20,11 @@ const contactFormEntry = async (firstName, lastName, email, message) => {
         message: message,
       },
     });
-    await newcontact.save();
 
-    return contact;
+    return newContact;
+  } catch (error) {
+    console.error.apply("Error processing contact form entry: ", error);
+    throw error;
   } finally {
     await prisma.$disconnect();
   }
