fb936dc4ce55e513dc61d5fc6345a71dab48d81d
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-23 15:38:02 +0200

    back to port 3000

--- starter/src/index.js
+++ starter/src/index.js
@@ -69,7 +69,7 @@ app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorHandler);
 
-const port = process.env.POST || 4000;
+const port = process.env.POST || 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
